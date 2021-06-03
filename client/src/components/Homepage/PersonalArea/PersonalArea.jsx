809f37b85184400f5a2cfb2b4eec0cdc3bd7c00f
Yaroslav <serafim90@mail.ru>
2021-06-03 17:37:52 +0300

    fixed calendar bugs

--- client/src/components/Homepage/PersonalArea/PersonalArea.jsx
+++ client/src/components/Homepage/PersonalArea/PersonalArea.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import style from './PersonalArea.module.css'
 import { Row, Col } from 'antd';
+import CalendarPage from './CalendarPage/CalendarPage';
+
 
 const PersonalArea = () => {
+
+  
   return (
     <div>
        <Row>
           <Col className={style.right_col} span={18} push={6}>
-            RIGHT COL
+            <CalendarPage /> 
           </Col>
           <Col className={style.left_col}  span={6} pull={18}>
             LEFt COL
