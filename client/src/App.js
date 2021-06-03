4f1a8f334ad92fe99af5b8f1c33feb6c3f7de3ca
Anton <kovshik@mail.ru>
2021-06-03 17:58:07 +0300

    0.1 beta doctor-card

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import CardDoctorPage from './components/cardDoctorPage/cardDoctorPage';
 import Homepage from './components/Homepage/Homepage';
 
 function App() {
   return (
     <div className="App">
       <Homepage />
+      <CardDoctorPage/>
     </div>
   );
 }
