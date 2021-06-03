0d7f12e3e005c8923c112bccf33ab5d76f4cadd3
Yaroslav <serafim90@mail.ru>
2021-06-03 13:05:19 +0300

    add Ant Design

--- client/src/components/Homepage/PersonalArea/PersonalArea.jsx
+++ client/src/components/Homepage/PersonalArea/PersonalArea.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import style from './PersonalArea.module.css'
+import { Row, Col } from 'antd';
 
 const PersonalArea = () => {
   return (
     <div>
-      
+       <Row>
+          <Col className={style.right_col} span={18} push={6}>
+            RIGHT COL
+          </Col>
+          <Col className={style.left_col}  span={6} pull={18}>
+            LEFt COL
+          </Col>
+      </Row>
     </div>
   )
 }
