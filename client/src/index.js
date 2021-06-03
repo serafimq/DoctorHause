5ef5309c0ae4249563bed0550fd0923474e268e2
Evgeny Kolesnikov <48915901+Ricorder@users.noreply.github.com>
2021-06-03 14:19:22 +0300

    Prepare

--- client/src/index.js
+++ client/src/index.js
@@ -2,17 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'antd/dist/antd.css'
 import './index.css';
-import App from './App';
-
-
+import App from './App.js';
 
 ReactDOM.render(
   <React.StrictMode>
-
     <App />
-
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-
