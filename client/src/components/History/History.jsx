ec67952fad2851547f92976a90340c3f2a4f378e
Elbrus <elbrus@pashaz.localdomain>
2021-06-08 13:02:00 +0300

    historyPage filter + mapPage changes 13.01 08/06

--- client/src/components/History/History.jsx
+++ client/src/components/History/History.jsx
@@ -1,23 +1,30 @@
 import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { setAllHistoryThunk } from '../../redux/actionCreators/historyAC'
-import { Card, Switch, Skeleton, Avatar } from 'antd';
+import { Card, Switch, Skeleton, Avatar, Col, Row } from 'antd';
 import styleHistory from './History.module.css'
 import { CloseOutlined, CheckOutlined } from '@ant-design/icons';
+import { setAllEvents } from '../../redux/actionCreators/eventsAC';
+const { Meta } = Card;
 
 const History = () => {
 
   const id = useSelector(state => state.user.id)
   const history = useSelector(state => state.history)
+  const events = useSelector(state => state.events)
+
   const dispatch = useDispatch()
 
   console.log(history, 'history');
+  console.log(events, 'events');
 
   useEffect(() => {
     dispatch(setAllHistoryThunk(id))
+    dispatch(setAllEvents(id))
   }, [])
 
   const [loading, setLoading] = useState(true)
+  const [filteredHistory, setFilteredHistory] = useState(history)
 
   // onChange = checked => {
   //   this.setState({ loading: !checked });
@@ -27,42 +34,118 @@ const History = () => {
     setLoading(!loading)
   }
 
-  console.log(loading);
+  const style = { background: '#0092ff', padding: '8px 0' };
+  const filterHandler = (id, problem) => {
+    // const filtHistory = history.reduce((acc, el) => {
+    //   const currentEl = el.events.filter(event => {
+    //     console.log({ problem });
+    //     console.log(event.problem);
+    //     return event.problem === problem
+    //   })
+    //   el.events = currentEl;
+    //   if (currentEl.length) {
+    //     acc.push(el)
+    //   }
+    //   return acc
+    // }, [])
+    const result = []
+    const newHistory = JSON.parse(JSON.stringify(history)) 
+    const filtHistory = newHistory?.map(el => {
+      const currentEl = el.events?.filter(event => event.problem === problem)
+      el.events = currentEl
+      console.log(el.events);
+      return el
+    })
+    filtHistory?.forEach(el => {
+      if (el.events.length) result.push(el)
+    })
+    console.log({ result });
+    setFilteredHistory(result)
+    console.log(filteredHistory, 'filteredHistory');
+  }
+
 
   return (
-    <>
-    <div className={styleHistory.switch_right}> Показывать загруженные файлы? &nbsp;
-      <Switch checked={!loading}  onChange={onChangeSwitch} checkedChildren={<CheckOutlined />} unCheckedChildren={<CloseOutlined />} defaultChecked/>
-    </div>
-    
-    <div className={styleHistory.list}>
-      {
-        history ? history.map(el =>
+    <div className={styleHistory.historyBox}>
+      <div className={styleHistory.switch_right}> Показывать загруженные файлы? &nbsp;
+      <Switch checked={!loading} onChange={onChangeSwitch} checkedChildren={<CheckOutlined />} unCheckedChildren={<CloseOutlined />} defaultChecked />
+      </div>
+      <div>
 
-          <Card
-            style={{ marginTop: 16, width: 700 }}
-            type="inner"
-            // 
-            title={`Жалоба:${el.events[0].problem}`}
-            extra={<a href="#">More</a>}
-            >
-            {/* <div> Показывать файлы:   
-            
-            </div> */}
-            <div >Выписанные рецепты: {el.prescription}  </div>
-            <div >Требуемые анализы: {el.analyzes}</div>
-            <div >{el.prescription}</div>
+        <Row className={styleHistory.row}>
+          <Col >
+            <div>
               {
-              el.imagePath && el.imagePath.map(imgP => <Skeleton loading={loading}><Avatar  shape="square" loading={loading} style={{marginTop: 5, width: 500, height: 500}} src={`/img/${imgP}`} alt="FOTO NE OTOBRACHAETSYA"></Avatar></Skeleton>)
-              } 
-          </Card>
-        )
-          :
-          <p>Истории болезней пока нет</p>
-      }
+                events.length ? events.map(el =>
+                  <div onClick={() => filterHandler(id, el.problem)}>
+                    <Card
+                      style={{ marginTop: 16, width: 600 }}
+                      type="inner"
+                      //
+                      title={`Жалоба: ${el.problem}`}
+                    >
+
+                      <div>Поликлинника: {el.hospital}</div>
+                      <div>Имя и Фамилия врача: {el.firstLastName}</div>
+                      <div>Специальность врача: {el.specialization}</div>
+                      <Meta className={styleHistory.date_description} description={`Дата приема: ${el.dateTime.replace('-', '/').replace('-', '/').replace('T', ' ').substring(0, 16)}`} />
+                    </Card>
+                  </div>
+                )
+                  :
+                  <p> Никаких запесей в календаре нет</p>
+
+              }
+            </div>
+
+          </Col>
+          <Col >
+            <div>
+              {
+                filteredHistory ? filteredHistory.map(el =>
+
+                  <Card
+
+                    type="inner"
+                    // 
+                    style={{ marginTop: 16, width: 600 }}
+                    title={`Жалоба:${el.events[0].problem}`}
+                    extra={<a href="#">More</a>}
+                  >
+                    <div >Выписанные рецепты: {el.prescription}  </div>
+                    <div >Требуемые анализы: {el.analyzes}</div>
+                    <div >{el.prescription}</div>
+                    {
+                      el.imagePath && el.imagePath.map(imgP => <Skeleton loading={loading}><Avatar shape="square" loading={loading} style={{ marginTop: 5, width: 500, height: 500 }} src={`/img/${imgP}`} alt="FOTO NE OTOBRACHAETSYA"></Avatar></Skeleton>)
+                    }
+                  </Card>
+                )
+                  :
+                  <p>Истории болезней пока нет</p>
+              }
+            </div>
+
+          </Col>
+        </Row>
+      </div>
+
+
+      {/* <div className={styleHistory.list}> */}
+
     </div>
-    </>
   )
 }
+
+{/* <Row>
+          <Col className={style.right_col} span={18} push={6}>
+            {visibleComponents === 0 && <CalendarPage /> }
+            {visibleComponents === 1 && <History /> }
+            {visibleComponents === 3 && <MapPage /> }
+            {/* {visibleComponents === 4 && <CalendarPage /> } */}
+//   </Col>
+//   <Col className={style.left_col} span={6} pull={18}>
+//     <CardProfile setVisibleComponents={setVisibleComponents}/>
+//   </Col>
+// </Row> */}
 
 export default History
--- client/src/components/Homepage/PersonalArea/CalendarPage/CalendarPage.jsx
+++ client/src/components/Homepage/PersonalArea/CalendarPage/CalendarPage.jsx
@@ -10,128 +10,128 @@ const CalendarPage = () => {
 
   const events = useSelector(state => state.events)
   const id = useSelector(state => state.user.id)
-   
+
   const dispatch = useDispatch()
   useEffect(() => {
     dispatch(setAllEvents(id))
-   }, [])
-   
+  }, [])
 
-   function name(params) {
-     
-     //  let arr =[]
-     //  for (let i = 1; i <= 31; i++) {
-     //    arr.push({i: events.filter(el => el.num === 1)})
-     //  }
-     
-       // const filter = Array(12).fill(Array(31).fill([])) 
-   
-       // events.forEach(el => {
-       //   if (el.month) {
-       //     filter[el.month][el.day - 1] = [...filter[el.month][el.day - 1], el]
-           // console.log(filter[el.month], el.month, "this month");
-           // console.log(filter[el.month][el.day - 1], el.month, el.day - 1);
-       //   }
-       // })
-       // const filter1 = events.filter(el => el.num === 1)
-       // const filter2  
-   
-     // function getListData(value) {
-     //   if (filter[value.month()]) {
-     //     // console.log(value.month(), value.date(), "aaaaaa", filter[value.month()][value.date() - 1]);
-     //     return filter[value.month()][value.date() - 1]
-     //   } 
-     //   return []
-       // console.log(value.date(), value.month());
-         // let listData;
-         // switch (value.date()) {
-         //   case 1: listData = filter1 
-         //   break;
-         //   case 2: listData = filter2
-         //     break;
-         //   case 3: listData = filter3
-         //     break;
-         //   case 4: listData = filter4
-         //     break;
-         //   case 5: listData = filter5
-         //     break;
-         //   case 6: listData = filter6
-         //     break;
-         //   case 7: listData = filter7
-         //     break;
-         //   case 8: listData = filter8
-         //     break;
-         //   case 9: listData = filter9
-         //     break;
-         //   case 10: listData = filter10
-         //     break;
-         //   case 11: listData = filter11
-         //     break;
-         //   case 12: listData = filter12
-         //     break;
-         //   case 13: listData = filter13
-         //     break;
-         //   case 14: listData = filter14
-         //     break;
-         //   case 15: listData = filter15
-         //     break;
-         //   case 16: listData = filter16
-         //     break;
-         //   case 17: listData = filter17
-         //     break;
-         //   case 18: listData = filter18
-         //     break;
-         //   case 19: listData = filter19
-         //     break;
-         //   case 20: listData = filter20
-         //     break;
-         //   case 21: listData = filter21
-         //     break;
-         //   case 22: listData = filter22
-         //     break;
-         //   case 23: listData = filter23
-         //     break;
-         //   case 24: listData = filter24
-         //     break;
-         //   case 25: listData = filter25
-         //     break;
-         //   case 26: listData = filter26
-         //     break;
-         //   case 27: listData = filter27
-         //     break;
-         //   case 28: listData = filter28
-         //     break;
-         //   case 29: listData = filter29
-         //     break;
-         //   case 30: listData = filter30
-         //     break;
-         //   case 31: listData = filter31
-         //     break;
-          
-         //     default:
-         //     }
-         //     return listData || [];
-           
-     // }
-   }
+
+  function name(params) {
+
+    //  let arr =[]
+    //  for (let i = 1; i <= 31; i++) {
+    //    arr.push({i: events.filter(el => el.num === 1)})
+    //  }
+
+    // const filter = Array(12).fill(Array(31).fill([])) 
+
+    // events.forEach(el => {
+    //   if (el.month) {
+    //     filter[el.month][el.day - 1] = [...filter[el.month][el.day - 1], el]
+    // console.log(filter[el.month], el.month, "this month");
+    // console.log(filter[el.month][el.day - 1], el.month, el.day - 1);
+    //   }
+    // })
+    // const filter1 = events.filter(el => el.num === 1)
+    // const filter2  
+
+    // function getListData(value) {
+    //   if (filter[value.month()]) {
+    //     // console.log(value.month(), value.date(), "aaaaaa", filter[value.month()][value.date() - 1]);
+    //     return filter[value.month()][value.date() - 1]
+    //   } 
+    //   return []
+    // console.log(value.date(), value.month());
+    // let listData;
+    // switch (value.date()) {
+    //   case 1: listData = filter1 
+    //   break;
+    //   case 2: listData = filter2
+    //     break;
+    //   case 3: listData = filter3
+    //     break;
+    //   case 4: listData = filter4
+    //     break;
+    //   case 5: listData = filter5
+    //     break;
+    //   case 6: listData = filter6
+    //     break;
+    //   case 7: listData = filter7
+    //     break;
+    //   case 8: listData = filter8
+    //     break;
+    //   case 9: listData = filter9
+    //     break;
+    //   case 10: listData = filter10
+    //     break;
+    //   case 11: listData = filter11
+    //     break;
+    //   case 12: listData = filter12
+    //     break;
+    //   case 13: listData = filter13
+    //     break;
+    //   case 14: listData = filter14
+    //     break;
+    //   case 15: listData = filter15
+    //     break;
+    //   case 16: listData = filter16
+    //     break;
+    //   case 17: listData = filter17
+    //     break;
+    //   case 18: listData = filter18
+    //     break;
+    //   case 19: listData = filter19
+    //     break;
+    //   case 20: listData = filter20
+    //     break;
+    //   case 21: listData = filter21
+    //     break;
+    //   case 22: listData = filter22
+    //     break;
+    //   case 23: listData = filter23
+    //     break;
+    //   case 24: listData = filter24
+    //     break;
+    //   case 25: listData = filter25
+    //     break;
+    //   case 26: listData = filter26
+    //     break;
+    //   case 27: listData = filter27
+    //     break;
+    //   case 28: listData = filter28
+    //     break;
+    //   case 29: listData = filter29
+    //     break;
+    //   case 30: listData = filter30
+    //     break;
+    //   case 31: listData = filter31
+    //     break;
+
+    //     default:
+    //     }
+    //     return listData || [];
+
+    // }
+  }
 
   function dateCellRender(value) {
     const listData = events.filter(i => i.date === value.format('YYYY-MM-DD'));
-    
+
     return (
-      <ul  className={style.events}>
-        
+      <ul className={style.events}>
+
         {listData.map(item => (
-          <li  key={item._id}>
+          <li key={item._id}>
             <div>
-            <Badge status={item.hospital} text={item.hospital} />
+              <Badge status={item.hospital} text={item.hospital} />
             </div>
             <div>
               <Badge status={item.hospital} text={item.specialization} />
             </div>
           </li>
         ))}
-       
+
       </ul>
     );
   }
@@ -217,7 +217,7 @@ const CalendarPage = () => {
                       <p>Имя и Фамилия врача: {el.firstLastName}</p>
                       <p>Специализация: {el.specialization}</p>
                       <p>Адрес: {el.address}</p>
-                      <ResultModal idEvent={el._id}/>
+                      <ResultModal idEvent={el._id} />
                     </Card>
 
                   </>)
